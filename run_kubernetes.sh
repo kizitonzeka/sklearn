a3669c469d3efe1706c2afad2bf9c6810a452c12
kizitonzeka <kizzyche@ymail.com>
2021-08-31 10:16:43 +0100

    Build and deploy kubernetes cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath="kizitonzeka/sklearn:v1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run sklearn\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=sklearn
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward sklearn 8000:80
